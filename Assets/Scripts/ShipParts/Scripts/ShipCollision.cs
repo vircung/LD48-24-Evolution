f6d459548536e08f58f6f43fbf25435430c45d57
Jacek Nakonieczny <jacek.nakonieczny@gmail.com>
2012-08-25 18:45:18 +0200

    Forgot about that.

--- Assets/Scripts/ShipParts/Scripts/ShipCollision.cs
+++ Assets/Scripts/ShipParts/Scripts/ShipCollision.cs
@@ -7,11 +7,11 @@ public class ShipCollision : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
+        Player p = transform.parent.GetComponent<Player>();
         Hull h = p.ship.map[this] as Hull;
 
         if (other.tag == "Enemy")
         {
-            Player p = transform.parent.GetComponent<Player>();
 
             Enemy1 es = other.GetComponent<Enemy1>();
             h.TakeDamage(es.enemy.damage);
